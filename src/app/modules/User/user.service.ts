8b537ffb9684686f26954ae67f2b8e7747a7637a
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-04-18 15:26:12 +0600

    feat: add bcrypt for password hashing and update user service to hash passwords on admin creation

--- src/app/modules/User/user.service.ts
+++ src/app/modules/User/user.service.ts
@@ -1,15 +1,16 @@
 import { PrismaClient, UserRole } from "@prisma/client";
-
+import bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient();
 
 const createAdminInToDB = async (data: any) => {
 
+    const hashedPassword = await bcrypt.hash(data.password,12);
     const userData = {
         email: data.admin.email,
-        password: data.password,
+        password: hashedPassword,
         role: UserRole.ADMIN
-    }
+    };
 
     const result = await prisma.$transaction(async (tClient) => {
         // create user
