2c1fba770ef1bcae5dfe788883aeb011297c2102
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-04-19 13:19:39 +0600

    refactor getAllAdmin to use dynamic where conditions for improved search functionality

--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -1,26 +1,35 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 const getAllAdmin = async (params: any) => {
+
+    const andConditions : Prisma.AdminWhereInput[] = [];
+
+    if (params.searchTerm) {
+        andConditions.push({
+            OR: [
+                {
+                    name: {
+                        contains: params.searchTerm,
+                        mode: 'insensitive',
+                    }
+                },
+                {
+                    email: {
+                        contains: params.searchTerm,
+                        mode: 'insensitive',
+                    }
+                }
+            ]
+        })
+    }
+
+    const whereConditions : Prisma.AdminWhereInput  = {AND : andConditions}
+
     try {
         const result = await prisma.admin.findMany({
-            where: {
-                OR: [
-                    {
-                        name: {
-                            contains: params.searchTerm,
-                            mode: 'insensitive',
-                        }
-                    },
-                    {
-                        email: {
-                            contains: params.searchTerm,
-                            mode: 'insensitive',
-                        }
-                    }
-                ]
-            }
+            where: whereConditions
         });
         return result;
     } catch (error) {
