0c9f52beebadaa7a2a267f7900defbc99c6718fa
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-04-23 23:55:51 +0600

    Add Cloudinary integration for image uploads and remove unused screenshot

--- src/app/helpers/fileUploader.ts
+++ src/app/helpers/fileUploader.ts
@@ -1,5 +1,6 @@
 import multer from "multer";
 import path from "path";
+import { v2 as cloudinary } from 'cloudinary';
 
 
 const storage = multer.diskStorage({
@@ -13,6 +14,28 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage: storage });
+
+
+
+// Configuration
+cloudinary.config({
+    cloud_name: 'dbgiyghuf',
+    api_key: '815324725584527',
+    api_secret: '<your_api_secret>' // Click 'View API Keys' above to copy your API secret
+});
+
+// Upload an image
+const uploadResult = cloudinary.uploader
+    .upload(
+        'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
+        public_id: 'shoes',
+    }
+    )
+    .catch((error) => {
+        console.log(error);
+    });
+
+console.log(uploadResult);
 
 export const fileUploader = {
     upload
