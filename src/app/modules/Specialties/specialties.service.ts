0fe3ef6e687189b3b737e07689963100efa0e895
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-04-30 22:13:26 +0600

    Refactor specialties service to handle file uploads and create specialties in the database

--- src/app/modules/Specialties/specialties.service.ts
+++ src/app/modules/Specialties/specialties.service.ts
@@ -1,8 +1,29 @@
 import { Request } from "express";
+import { fileUploader } from "../../helpers/fileUploader";
+import prisma from "../../utils/prisma";
 
 
 const insertIntoDB = async (req: Request) => {
-    return req.body;
+
+    const file = req.file;
+    if (file) {
+        const uploadToCloudinary = await fileUploader.uploadToCloudinary(file);
+        req.body.icon = uploadToCloudinary?.secure_url;
+    };
+
+    console.log( {
+        title :  req.body.title,
+        icon : req.body.icon
+    });
+
+    const result = await prisma.specialties.create({
+        data: {
+            title :  req.body.title,
+            icon : req.body.icon
+        }
+    })
+
+    return result;
 };
 
 
