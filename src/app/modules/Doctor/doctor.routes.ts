d3549865259f28c6ef19c1434bb5cfa3156c6636
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-04-30 23:37:47 +0600

    Implement Doctor routes and service methods for CRUD operations

--- src/app/modules/Doctor/doctor.routes.ts
+++ src/app/modules/Doctor/doctor.routes.ts
@@ -1,7 +1,39 @@
 import { Router } from "express";
+import auth from "../../middlewares/auth";
+import { UserRole } from "@prisma/client";
 
 
 const router = Router();
 
+router.get(
+    '/',
+    auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
+
+);
+
+router.get(
+    '/:id',
+    auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
+
+);
+
+router.patch(
+    '/:id',
+    auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
+
+
+);
+
+router.delete(
+    '/:id',
+    auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
+
+);
+
+router.delete(
+    '/soft/:id',
+    auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
+
+);
 
 export const DoctorRouter = router;
--- src/app/modules/Doctor/doctor.service.ts
+++ src/app/modules/Doctor/doctor.service.ts
@@ -1,8 +1,53 @@
+import { StatusCodes } from "http-status-codes";
+import AppError from "../../errors/appError";
+import prisma from "../../utils/prisma";
 
+const getDoctorsFormDB = async () => {
+    return prisma.doctor.findMany();
+};
 
+const getDoctorById = async (id: string) => {
+    const doctor = await prisma.doctor.findUnique({
+        where: {
+            id
+        }
+    });
 
+    if (!doctor) {
+        throw new AppError(
+            StatusCodes.NOT_FOUND,
+            'doctor not found!'
+        );
+    };
 
+    return doctor;
+};
+
+const deleteDoctor = async (id: string) => {
+    const isExist = await prisma.doctor.findUnique({
+        where: {
+            id
+        }
+    });
+    if (!isExist) {
+        throw new AppError(
+            StatusCodes.NOT_FOUND,
+            'doctor not found!'
+        );
+    };
+
+    return prisma.doctor.update({
+        where: {
+            id
+        },
+        data: {
+            isDeleted: true
+        }
+    });
+};
 
 export const doctorServices = {
-
+    getDoctorsFormDB,
+    getDoctorById,
+    deleteDoctor
 };
