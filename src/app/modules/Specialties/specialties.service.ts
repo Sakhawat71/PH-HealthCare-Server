d7ead29d52f75a419a021cf4ba37a028be7d8444
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-04-30 22:37:19 +0600

    Refactor specialties service to include type definitions for file handling and improve type safety

--- src/app/modules/Specialties/specialties.service.ts
+++ src/app/modules/Specialties/specialties.service.ts
@@ -1,20 +1,21 @@
 import { Request } from "express";
 import { fileUploader } from "../../helpers/fileUploader";
 import prisma from "../../utils/prisma";
+import { ICloudinaryResponse, IFile } from "../../interfaces/file";
 
 
 const insertIntoDB = async (req: Request) => {
 
-    const file = req.file;
+    const file = req.file as IFile;
     if (file) {
-        const uploadToCloudinary = await fileUploader.uploadToCloudinary(file);
+        const uploadToCloudinary = await fileUploader.uploadToCloudinary(file) as ICloudinaryResponse;
         req.body.icon = uploadToCloudinary?.secure_url;
     };
 
     const result = await prisma.specialties.create({
         data: {
-            title :  req.body.title,
-            icon : req.body.icon
+            title: req.body.title,
+            icon: req.body.icon
         }
     })
 
