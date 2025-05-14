76531ae381e5a7891b05058bc671b1855a4524d0
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-05-14 22:12:31 +0400

    fix controller

--- src/app/modules/DoctorSchedule/doctorSchedule.controller.ts
+++ src/app/modules/DoctorSchedule/doctorSchedule.controller.ts
@@ -3,8 +3,12 @@ import catchAsync from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/sendResponse";
 import { DoctorScheduleServices } from "./doctorSchedule.service";
 import { StatusCodes } from "http-status-codes";
+import { IAuthUser } from "../../interfaces/common";
 
-const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
+const insertIntoDB = catchAsync(async (
+    req: Request & { user?: IAuthUser },
+    res: Response
+) => {
     const result = await DoctorScheduleServices.insertIntoDB(req);
     sendResponse(res, {
         statusCode: StatusCodes.OK,
--- src/app/modules/DoctorSchedule/doctorSchedule.service.ts
+++ src/app/modules/DoctorSchedule/doctorSchedule.service.ts
@@ -2,13 +2,15 @@ import { Request } from "express";
 import prisma from "../../utils/prisma";
 
 const insertIntoDB = async (req: Request) => {
-    const doctorData = await prisma.doctor.findUnique({
+    const doctorData = await prisma.doctor.findUniqueOrThrow({
         where: {
             email : req.user.email,
-        }
+        },
     });
 
-    // console.log(req.body.scheduleIds);
+    const doctorScheduleData = await 
+
+    console.log(req.body.scheduleIds);
 
     return doctorData;
 };
